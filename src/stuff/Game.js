4554571021f4e5f105401529fd501d1eaea08d70
Michael Wandtke <Michael.Wandtke@gmail.com>
2021-02-05 12:54:44 +0100

    Make new game clearer, prevent overriding stones

--- src/stuff/Game.js
+++ src/stuff/Game.js
@@ -24,10 +24,9 @@ export default function Game() {
     return (<div>
         Hello, my darling. Have a great game. And please, have fun playing it.
         <br />It is {named(currentPlayer)}'s turn to play.
+        <Button onClick={() => setCurrentPlayer(nextPlayer)} variant="contained" color="primary">PASS</Button>
         <br />
-        <Button onClick={() => setCurrentPlayer(nextPlayer)}>Click</Button>
-        <br />
-        <TextField onChange={(event) => boardSizeChange(event, setRows)}>Hello.</TextField>
+        Create a new Game in size: <TextField onChange={(event) => newGame(event, setRows, setCurrentPlayer)}>Hello.</TextField>
         <Board rows={rows} clicked={(id) => clicked(id, rows, setRows, currentPlayer, () => setCurrentPlayer(nextPlayer))}/>
     </div>)
 }
@@ -39,9 +38,11 @@ function named(currentPlayer) {
 }
 
 
-function boardSizeChange(event, setRows) {
-    if (event.target.value >= 2)
+function newGame(event, setRows, setCurrentPlayer) {
+    if (event.target.value >= 2) {
+        setCurrentPlayer(player.black)
         setRows(build_rows(parseInt(event.target.value)))
+    }
 }
 
 function clicked(id, rows, setRows, currentPlayer, nextPlayer) {
@@ -53,7 +54,7 @@ function replaceIdWithCurrentPlayer(id, rows, currentPlayer, nextPlayer) {
 }
 
 function replaceIfClicked(cell, id, currentPlayer, nextPlayer) {
-    if (cell.id === id)
+    if (cell.id === id && cell.owner === owner_type.empty)
         if (currentPlayer === player.black) {
             nextPlayer()
             return new Owner(cell.id, cell.cellType, owner_type.black)
